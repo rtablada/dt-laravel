147eba187938717995a2a15a77b7df6902743580
Ryan Tablada <ryan.tablada@gmail.com>
2015-11-22 21:24:20 -0600

    Update navbar for signup and login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,14 @@
 @extends('_layout')
 
 @section('content')
+<div class="navbar">
+  <div class="navbar-inner navbar-on-center">
+    <div class="left"></div>
+    <div class="center sliding">Dog Trainer</div>
+    <div class="right"></div>
+  </div>
+</div>
+
 {!! Form::open(['route' => 'login.store']) !!}
   <div class="list-block">
     <ul>
